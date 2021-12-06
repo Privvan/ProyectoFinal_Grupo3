2bdfcc29839e9753d955f702f4227f8f8c8acb03
Privvan <95545727+Privvan@users.noreply.github.com>
2021-12-05 22:43:54 -0600

    Create cls_Telefono_DAL.cs

---
+++ DAL/Catalogos/cls_Telefono_DAL.cs
@@ -0,0 +1,22 @@
+namespace DAL
+{
+    public class cls_Telefono_DAL
+    {
+        private DataTable _dtTelefono;
+
+        private int _iIDTelef;
+
+        private int _iClienteID;
+
+        private int _iNumTelef;
+        private string  _sMsjError;
+        private char _cBanderaCxn;
+
+        public DataTable dtTelefonos { get => _dtTelefono; set => _dtTelefono = value; }
+        public int IDTelef { get => _iIDTelef; set => _iIDTelef = value; }
+        public int IdCliente { get => _iClienteID; set => _iClienteID = value; }
+        public int NumeroTelefono { get => _iNumTelef; set => _iNumTelef = value; }
+        public string sMsjError { get => _sMsjError; set => _sMsjError = value; }
+        public char cBanderaAxn { get => _cBanderaCxn; set => _cBanderaCxn = value; }
+    }
+}
